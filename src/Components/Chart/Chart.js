817d99f155ef811fffc79b50dbfc0db081258f3b
Jayden Leuciuc <leuciucjayden@gmail.com>
2020-11-15 12:16:43 -0500

    Pulled first data from api

---
+++ src/Components/Chart/Chart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './chart.scss'
+function Chart() {
+    return (
+        <div>
+            a
+        </div>
+    )
+}
+
+export default Chart
---
+++ src/Components/CountryPicker/CountryPicker.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './countrypicker.scss'
+function CountryPicker() {
+    return (
+        <div>
+            b
+        </div>
+    )
+}
+
+export default CountryPicker

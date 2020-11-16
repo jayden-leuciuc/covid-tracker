93402611ea7fa36bd7f554560dcb5a990a84d527
Jayden Leuciuc <leuciucjayden@gmail.com>
2020-11-15 23:10:04 -0500

    Added country picker and chart(broken)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
-import React from 'react'
-import './app.scss';
-import {Cards, Chart, CountryPicker} from './Components'
-import {fetchData} from './api'
+import React from 'react';
 
-class App extends React.Component{
+import { Cards, CountryPicker, Chart } from './Components';
+import { fetchData } from './api/';
+import styles from './App.module.css';
+
+import image from './images/image.png';
+
+class App extends React.Component {
   state = {
-    data:{},
+    data: {},
+    country: '',
   }
 
-  async componentDidMount ()  {
-    const fetchedData = await fetchData();
-    this.setState({data: fetchedData})
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
   }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  }
+
   render() {
-    const {data} = this.state;
+    const { data, country } = this.state;
 
     return (
-      <div className="container">
-          <Cards data={data}/>
-          <CountryPicker />
-          <Chart />
+      <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} /> 
       </div>
     );
   }
-  }
-  
+}
 
 export default App;
--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,42 +1,41 @@
-import React from 'react'
-import './cards.scss'
-import {Card, CardContent, Typography, Grid} from '@material-ui/core'
-import CountUp from 'react-countUp'
+import React from 'react';
+import { Typography, Grid } from '@material-ui/core';
+import CardComponent from './Card/Card';
+import styles from './Cards.module.css';
 
-function Cards({data: {confirmed, recovered, deaths, lastUpdate}}) {
-    if(!confirmed) {
-        return 'Loading...'
-    }
-    return (
-       <div className="container">
-           <Grid container spacing={3} justify="center">
-            <Grid item component={Card}>
-                <CardContent>
-                    <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                    <Typography variant="h5">{confirmed.value}</Typography>
-                    <Typography color="textSecondary">REAL DATE</Typography>
-                    <Typography variant="body2">Number of active cases of COVID-19</Typography>
-                </CardContent>
-            </Grid>
-            <Grid item component={Card}>
-                <CardContent>
-                    <Typography color="textSecondary" gutterBottom>Recovered</Typography>
-                    <Typography variant="h5">REAL DATA</Typography>
-                    <Typography color="textSecondary">REAL DATE</Typography>
-                    <Typography variant="body2">Number of recoveries from COVID-19</Typography>
-                </CardContent>
-            </Grid>
-            <Grid item component={Card}>
-                <CardContent>
-                    <Typography color="textSecondary" gutterBottom>Deaths</Typography>
-                    <Typography variant="h5">REAL DATA</Typography>
-                    <Typography color="textSecondary">REAL DATE</Typography>
-                    <Typography variant="body2">Number of deaths caused by COVID-19</Typography>
-                </CardContent>
-            </Grid>
-           </Grid>
-       </div>
-    )
-}
+const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return 'Loading...';
+  }
 
-export default Cards
+  return (
+    <div className={styles.container}>
+        <Typography gutterBottom variant="h4" component="h2">Global</Typography>
+      <Grid container spacing={3} justify="center">
+        <CardComponent
+          className={styles.infected}
+          cardTitle="Infected"
+          value={confirmed.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Number of active cases from COVID-19."
+        />
+        <CardComponent
+          className={styles.recovered}
+          cardTitle="Recovered"
+          value={recovered.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Number of recoveries from COVID-19."
+        />
+        <CardComponent
+          className={styles.deaths}
+          cardTitle="Deaths"
+          value={deaths.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Number of deaths caused by COVID-19."
+        />
+      </Grid>
+    </div>
+  );
+};
+
+export default Info;
